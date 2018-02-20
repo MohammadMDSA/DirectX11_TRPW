0368a9260c35f03e7b4a1dce06a247d0b6f6b13d
MohammadMDSA <samimd.77@gmail.com>
2018-02-20 22:05:14 +0330

    Added CGame.cpp structure

---
+++ TRM/Game.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "Game.h"
+
+// Initializes and prepare Direct3D
+void CGame::Initialize()
+{
+
+}
+
+// Performs update to the game states
+void CGame::Update()
+{
+
+}
+
+// Renders a single frame of 3D graphics
+void CGame::Render()
+{
+
+}
