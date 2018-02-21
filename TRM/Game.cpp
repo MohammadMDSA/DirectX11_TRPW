85f37503cfce0ee82164ba95d30bae8c9d7a6fb0
MohammadMDSA <samimd.77@gmail.com>
2018-02-21 05:56:39 +0330

    Initialized device and device context

--- TRM/Game.cpp
+++ TRM/Game.cpp
@@ -4,7 +4,27 @@
 // Initializes and prepare Direct3D
 void CGame::Initialize()
 {
+	// Define temp pointer variables to the device and device context
+	ComPtr<ID3D11Device> tempDevice;
+	ComPtr<ID3D11DeviceContext> tempDeviceContext;
 
+	// Creating the device and device context object
+	D3D11CreateDevice(
+		nullptr,
+		D3D_DRIVER_TYPE_HARDWARE,
+		nullptr,
+		0,
+		nullptr,
+		0,
+		D3D11_SDK_VERSION,
+		&tempDevice,
+		nullptr,
+		&tempDeviceContext
+	);
+
+	// Convert the pointers from the DX 11 version to the DX 11.2 version
+	tempDevice.As(&m_Device);
+	tempDeviceContext.As(&m_DeviceContext);
 }
 
 // Performs update to the game states
--- TRM/Game.h
+++ TRM/Game.h
@@ -8,7 +8,10 @@ using namespace Windows::UI::Core;
 class CGame
 {
 public:
-	void Initialize();		// Starting up code
-	void Update();			// Code that manipulates the game, such as timer and input
-	void Render();			// Draw graphics
+	ComPtr<ID3D11Device1> m_Device;					// The device interface(Virtual representation of the video adaptor)
+	ComPtr<ID3D11DeviceContext1> m_DeviceContext;	// The control panel for the GPU
+
+	void Initialize();								// Starting up code
+	void Update();									// Code that manipulates the game, such as timer and input
+	void Render();									// Draw graphics
 };
